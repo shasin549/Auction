7591109111138c6bb2488bf4082006947c5d49c5
shasin549 <shasinsha7384@gmail.com>
2025-07-19 13:15:24 +0530

    Update auctioneer.js

--- public/auctioneer.js
+++ public/auctioneer.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Initialize Supabase client
-  const supabaseUrl = 'https://flwqvepusbjmgoovqvmi.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsd3F2ZXB1c2JqbWdvb3Zxdm1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MDY3MzMsImV4cCI6MjA2ODQ4MjczM30.or5cIl99nUDZceOKlFMnu8PCzLuCvXT5TBJvKTPSUvM';
-  const supabase = createClient(supabaseUrl, supabaseKey);
-
   const socket = io('https://auction-zfku.onrender.com', {
     transports: ['websocket', 'polling'],
     reconnectionAttempts: 5,
