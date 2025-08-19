90c21caf667eb68f4d9c17d8db74f688246133a8
shasin549 <shasinsha7384@gmail.com>
2025-08-19 18:48:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,62 +8,32 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let rooms = {};
+let auctionRoom = {
+  roomName: "Premier League Auction",
+  participants: [],
+  bidIncrement: 1000,
+  roomCode: "ABCD123"
+};
 
-io.on("connection", (socket) => {
-  socket.on("createRoom", ({ roomName, participants, bidIncrement }, callback) => {
-    const roomCode = Math.random().toString(36).substr(2, 6).toUpperCase();
-    rooms[roomCode] = {
-      name: roomName,
-      participants: [],
-      bidIncrement: parseInt(bidIncrement),
-      highestBid: 0,
-      highestBidder: null,
-      currentPlayer: null
-    };
-    socket.join(roomCode);
-    callback(roomCode);
+io.on("connection", socket => {
+
+  // Auctioneer requests info
+  socket.on("requestAuctioneerInfo", () => {
+    socket.emit("updateAuctioneerInfo", auctionRoom);
   });
 
-  socket.on("joinRoom", ({ name, roomCode }, callback) => {
-    if (rooms[roomCode]) {
-      rooms[roomCode].participants.push({ id: socket.id, name, won: [] });
-      socket.join(roomCode);
-      io.to(roomCode).emit("participantsUpdate", rooms[roomCode].participants);
-      callback(true);
-    } else {
-      callback(false);
-    }
+  // Bidder joins
+  socket.on("joinAsBidder", ({ name }) => {
+    auctionRoom.participants.push({ id: socket.id, name });
+    socket.emit("updateBidderInfo", { roomCode: auctionRoom.roomCode });
+    io.emit("updateAuctioneerInfo", auctionRoom); // update auctioneer live
   });
 
-  socket.on("startBidding", ({ roomCode, player }) => {
-    if (rooms[roomCode]) {
-      rooms[roomCode].currentPlayer = player;
-      rooms[roomCode].highestBid = player.value;
-      rooms[roomCode].highestBidder = null;
-      io.to(roomCode).emit("newPlayer", player);
-    }
-  });
-
-  socket.on("placeBid", ({ roomCode, name }) => {
-    const room = rooms[roomCode];
-    if (room) {
-      room.highestBid += room.bidIncrement;
-      room.highestBidder = name;
-      io.to(roomCode).emit("updateBid", room.highestBid);
-    }
-  });
-
-  socket.on("finalizeSale", (roomCode) => {
-    const room = rooms[roomCode];
-    if (room && room.highestBidder) {
-      const winner = room.participants.find(p => p.name === room.highestBidder);
-      if (winner) {
-        winner.won.push(room.currentPlayer);
-        io.to(roomCode).emit("playerWon", room.currentPlayer);
-      }
-    }
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    auctionRoom.participants = auctionRoom.participants.filter(p => p.id !== socket.id);
+    io.emit("updateAuctioneerInfo", auctionRoom);
   });
 });
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+server.listen(3000, () => console.log("Server running at http://localhost:3000"));
